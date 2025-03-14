960d7fb297b75af1f5d38eba26bcdd3434bbc095
Kékesi Ádám <dpteh5@inf.elte.hu>
2025-03-14 10:09:58 +0000

    Resolve "Model: Map"

---
+++ Assets/Model/Field.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Model
+{
+    public abstract class Field
+    {
+        public abstract bool CanDemolish { get; }
+
+        public abstract bool CanPlaceHere(Ground field);
+
+        public abstract bool CanPlaceHere(Plant field);
+
+        public abstract bool CanPlaceHere(Road field);
+
+        public abstract bool CanPlaceHere(Water field);
+
+        public abstract bool CanPlaceHere(Gate field);
+
+    }
+}
---
+++ Assets/Model/Map.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+namespace Safari.Model
+{
+    public class Map
+    {
+        public event EventHandler<GridPosition>? FieldChanged;
+
+        public GridPosition EntranceCoords { get; }
+
+        public GridPosition ExitCoords { get; }
+
+        public int SizeX => grid.GetLength(1);
+
+        public int SizeY => grid.GetLength(0);
+
+        private Field[,] grid;
+
+        public Map(Field[,] grid, GridPosition entranceCoords, GridPosition exitCoords)
+        {
+            this.grid = grid;
+            EntranceCoords = entranceCoords;
+            ExitCoords = exitCoords;
+        }
+
+        public void ChangeFieldAt(GridPosition position, Field newField)
+        {
+            grid[position.Y, position.X] = newField;
+            FieldChanged?.Invoke(this, position);
+        }
+
+        public Field FieldAt(GridPosition position)
+        {
+            return grid[position.Y, position.X];
+        }
+    }
+}
+
