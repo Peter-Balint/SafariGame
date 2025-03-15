87945b6813a7bf37d4dd00fd6d8a97df712a9e38
adamkekesi <kekesi.adam2003@gmail.com>
2025-03-16 00:47:23 +0100

    refactor: Rename BuilderGrid to ConstructionGrid

--- Assets/Scripts/View/World/ConstructionGrid/ConstructionCell.cs
+++ Assets/Scripts/View/World/ConstructionGrid/ConstructionCell.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Safari.View.World.BuilderGrid
+namespace Safari.View.World.ConstructionGrid
 {
-    public class BuilderCell : MonoBehaviour
+    public class ConstructionCell : MonoBehaviour
     {
 
         public bool IsForbidden
@@ -32,37 +32,29 @@ namespace Safari.View.World.BuilderGrid
 
         private bool isForbidden;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
-            meshRenderer = GetComponentInChildren<MeshRenderer>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
-
-        public void OnHoverEnter()
-        {
-            isHovered = true;
-            ChangeMaterial();
-        }
-
-        public void OnHoverLeave()
-        {
-            isHovered = false;
-            ChangeMaterial();
-        }
-
-        public void OnClick()
+        public void OnMouseDown()
         {
             if (enabled)
             {
                 Click?.Invoke();
             }
+        }
+
+        public void OnMouseEnter()
+        {
+            isHovered = true;
+            ChangeMaterial();
+        }
+
+        public void OnMouseExit()
+        {
+            isHovered = false;
+            ChangeMaterial();
+        }
+
+        private void Start()
+        {
+            meshRenderer = GetComponentInChildren<MeshRenderer>();
         }
 
         private void ChangeMaterial()
---
+++ Assets/Scripts/View/World/ConstructionGrid/ConstructionGridController.cs
@@ -0,0 +1,59 @@
+#nullable enable
+using Safari.Model;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using static Safari.View.World.Map.MapDisplay;
+
+namespace Safari.View.World.ConstructionGrid
+{
+    public class ConstructionGridController : MonoBehaviour
+    {
+        public GameObject ConstructionCellPrefab;
+
+        public UnityEvent<GridPosition> Click;
+
+        private ConstructionCell[,]? cells;
+
+        public void OnMapInitialized(MapInitializedEventArgs args)
+        {
+            if (cells != null)
+            {
+                throw new InvalidOperationException($"{nameof(ConstructionGridController)} received {nameof(OnMapInitialized)} event more than once");
+            }
+
+            cells = new ConstructionCell[SafariGame.Instance.Map.SizeZ, SafariGame.Instance.Map.SizeX];
+            foreach (var pair in args.Displayers)
+            {
+                var worldPos = pair.Key;
+                var fieldDislay = pair.Value;
+                GameObject obj = Instantiate(ConstructionCellPrefab,
+                                             worldPos,
+                                             Quaternion.identity,
+                                             new InstantiateParameters()
+                                             {
+                                                 parent = transform,
+                                                 worldSpace = true,
+                                             });
+                var cell = obj.GetComponentInChildren<ConstructionCell>();
+                cells[fieldDislay.Position.Z, fieldDislay.Position.X] = cell;
+                cell.Click.AddListener(() => OnCellClick(fieldDislay.Position));
+            }
+        }
+
+        public void Open()
+        {
+            enabled = true;
+        }
+
+        public void Close()
+        {
+            enabled = false;
+        }
+
+        private void OnCellClick(GridPosition position)
+        {
+            Click?.Invoke(position);
+        }
+    }
+}
