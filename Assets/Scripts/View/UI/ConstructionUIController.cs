1607a3dd825b209e8c640e92ed168b3358c16a88
adamkekesi <kekesi.adam2003@gmail.com>
2025-03-16 14:00:25 +0100

    feat: Construction UI skeleton + TextMeshPro init + custom font

---
+++ Assets/Scripts/View/UI/ConstructionUIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Safari.View.UI
+{
+    public class ConstructionUIController : MonoBehaviour
+    {
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+
+        }
+
+        public void OnDemolish()
+        {
+
+        }
+    }
+}
