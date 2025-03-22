597e90d8553f7e4845bdc4f635671ed3623da5fc
adamkekesi <kekesi.adam2003@gmail.com>
2025-03-22 22:18:14 +0100

    AnimalCollection

---
+++ Assets/Model/Animals/AnimalCollection.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Model.Assets.Model.Animals
+{
+    public class AnimalCollection
+    {
+        private List<Animal> animals;
+
+        public event EventHandler<Animal>? Added;
+
+        public event EventHandler<Animal>? Removed;
+
+        public ReadOnlyCollection<Animal> Animals => animals.AsReadOnly();
+
+        public AnimalCollection()
+        {
+            animals = new List<Animal>();
+        }
+
+        internal void AddAnimal(Animal animal)
+        {
+            animals.Add(animal);
+            animal.AnimalDied += OnAnimalDied;
+            Added?.Invoke(this, animal);
+        }
+
+        private void OnAnimalDied(object sender, EventArgs e)
+        {
+            if (sender is Animal animal)
+            {
+                RemoveAnimal(animal);
+            }
+        }
+
+        internal void RemoveAnimal(Animal animal)
+        {
+            animals.Remove(animal);
+            Removed?.Invoke(this, animal);
+        }
+
+    }
+}
