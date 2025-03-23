7fe92bfa20502c47fbae4ef2fdbbde21539dc652
Kékesi Ádám <kekesi.adam2003@gmail.com>
2025-03-23 23:59:07 +0100

    asd

---
+++ Assets/Scripts/View/World/Map/AnimalLocationReporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Safari.View.World.Map
+{
+    public class AnimalLocationReporter : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Animal"))
+            {
+                //var animal = other.gameObject.GetComponent<Animal>();
+                //animal.Movement.ReportLocation(new GridPosition(transform.position));
+            }
+        }
+    }
+}
