27b426408001bbd89a292e93c859bf824b932af2
Kékesi Ádám <kekesi.adam2003@gmail.com>
2025-03-23 22:31:45 +0100

    namescpace fix

--- Assets/Scripts/View/Animal/AnimalMovement.cs
+++ Assets/Scripts/View/Animal/AnimalMovement.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Safari.View.Animal
+namespace Safari.View.Animals
 {
     public class AnimalMovement : MonoBehaviour
     {
