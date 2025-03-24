d9c5116d65a954878f9770fec01ff251b20b50ce
Péter Bálint <peter.balint18@gmail.com>
2025-03-24 13:22:21 +0100

    updated diagrams with new classes (animal model/view, map view)

--- Assets/Model/Animals/Lion.cs
+++ Assets/Model/Animals/Lion.cs
@@ -10,9 +10,6 @@ namespace Safari.Model.Animals
         public Lion(Lion? leader) : base()
         {
             Leader = leader;
-            age = 0;
-            hunger = 0;
-            thirst = 0;
         }
     }
 }
