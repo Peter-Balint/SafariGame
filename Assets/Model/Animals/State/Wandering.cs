df0bdccef70d1880d9d484ce4d39e4fe081ee0d5
Kékesi Ádám <kekesi.adam2003@gmail.com>
2025-04-05 19:00:40 +0200

    upload

---
+++ Assets/Model/Animals/State/Wandering.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Model.Animals.State
+{
+    public class Wandering :Animals.State.State
+    { 
+    }
+}
