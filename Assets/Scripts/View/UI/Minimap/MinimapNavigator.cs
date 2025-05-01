9566270c67e2a8d9becd93802ff9c96dcab5ca6b
Péter Bálint <peter.balint18@gmail.com>
2025-05-01 12:20:52 +0200

    navigation via map click

--- Assets/Scripts/View/UI/Minimap/MinimapNavigator.cs
+++ Assets/Scripts/View/UI/Minimap/MinimapNavigator.cs
@@ -1,31 +1,32 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using Safari.Model;
+using Safari.Model.Map;
+using Safari.View.World.Map;
 namespace Safari.View.Minimap
 {
     public class MinimapNavigator : MonoBehaviour
     {
         public Camera MainCamera;
 
+        private int mapSizeX;
+        private int MapSizeZ;
 
-        void Start()
+        private void Start()
         {
-            //MainCamera.transform.position += new Vector3(-100, 0, -100);
+            mapSizeX = SafariGame.Instance.Map.SizeX;
+            MapSizeZ = SafariGame.Instance.Map.SizeZ;
         }
 
-        /*private void OnMouseEnter()
-        {
-            Debug.Log(Input.mousePosition);
-        }
-        private void OnMouseDown()
-        {
-            Debug.Log(Input.mousePosition);
-        }*/
         public void Onclicked()
         {
             var localPosition = Input.mousePosition - new Vector3(Screen.width - 175, Screen.height - 175, 0);
             Debug.Log(localPosition);
             localPosition = new Vector3(localPosition.x / 160, localPosition.y / 160, 0);
             Debug.Log(localPosition);
+
+            MainCamera.transform.position = new Vector3(localPosition.x * mapSizeX * mapSizeX /2, MainCamera.transform.position.y, localPosition.y * MapSizeZ * MapSizeZ /2 - MainCamera.transform.position.y );
         }
+
+        
     }
 }
