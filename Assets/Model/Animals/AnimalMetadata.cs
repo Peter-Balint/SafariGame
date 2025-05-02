2531bbd95f093c43580c2fb582af51df7d308c80
Kékesi Ádám <kekesi.adam2003@gmail.com>
2025-05-03 00:32:17 +0200

    upload

--- Assets/Model/Animals/AnimalMetadata.cs
+++ Assets/Model/Animals/AnimalMetadata.cs
@@ -6,6 +6,15 @@ namespace Safari.Model.Animals
     [Serializable]
     public class AnimalMetadata
     {
+        // minutes
+        public int TimeTillHungry { get; private set; }
+        // minutes
+        public int TimeTillThirsty { get; private set; }
+        // minutes
+        public int TimeTillStarvation { get; private set; }
+        // minutes
+        public int TimeTillDehydration { get; private set; }
+
         public static AnimalMetadata Default => new AnimalMetadata()
         {
             Price = 0,
