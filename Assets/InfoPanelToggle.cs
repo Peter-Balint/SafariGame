ca39c6415a978e2d940f634c74ac5a86a83e6ae1
Komornik Márton <pr2nl9@inf.elte.hu>
2025-05-03 11:16:17 +0000

    Feat/parkinfo ui

---
+++ Assets/InfoPanelToggle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InfoPanelToggle : MonoBehaviour
+{
+	public GameObject infoPanel;
+
+	public void ToggleInfoPanel()
+	{
+		if (infoPanel != null)
+			infoPanel.SetActive(!infoPanel.activeSelf);
+	}
+}
