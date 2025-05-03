7960d6dfa6425d7efefece629183f787b29318a5
adamkekesi <kekesi.adam2003@gmail.com>
2025-05-03 19:08:39 +0200

    asd

--- Assets/Model/Jeep/Jeep.cs
+++ Assets/Model/Jeep/Jeep.cs
@@ -7,27 +7,20 @@ namespace Safari.Model.Jeep
 {
     public class Jeep : IMoving
     {
-		public Vector3 Position { get; set; }
-		public MovementBehavior Movement;
+        public Vector3 Position { get; set; }
+        public MovementBehavior Movement;
 
+        public Jeep()
+        {
+            Movement = new MovementBehavior(this, Position);
+            Position = new Vector3(8, 0, 137); // it should be enterance tile
 
+        }
 
-		public Jeep()
-		{
-			Movement = new MovementBehavior(this, Position);
-			Position = new Vector3(8,0,137); // it should be enterance tile
-
-		}
-
-		public Jeep(Vector3 vec3)
-		{
-			Movement = new MovementBehavior(this, Position);
-			Position = vec3;
-		}
-
-
-
-
-
-	}
+        public Jeep(Vector3 vec3)
+        {
+            Movement = new MovementBehavior(this, Position);
+            Position = vec3;
+        }
+    }
 }
