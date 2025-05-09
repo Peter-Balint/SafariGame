2c6606f4e79d14b57a13723bdcaebfa450d7cf78
Péter Bálint <peter.balint18@gmail.com>
2025-05-09 15:11:35 +0200

    files created

---
+++ Assets/Model/Hunters/HunterCollection.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Safari.Model
+{
+    public class HunterCollection
+    {
+    
+    }
+}
---
+++ Assets/Scripts/View/Hunters/HunterDisplay.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Safari.View
+{
+    public class HunterDisplay : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
